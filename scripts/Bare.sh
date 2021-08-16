e5562d59d10ec2c32399093d0049f50ccc7564f7
ninodemeterko <ninodemeterko@hotmail.com>
2021-08-16 13:23:24 -0300

    added readme and changed bare repository script

--- scripts/Bare.sh
+++ scripts/Bare.sh
@@ -6,3 +6,4 @@ git clone --bare https://github.com/ninodemeterko/dotfiles $HOME/.dotfiles
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
 echo ".dotfiles" >> .gitignore
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME reset --hard HEAD
+echo use \"/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME\" to manage the repository, you can alias it to anything
