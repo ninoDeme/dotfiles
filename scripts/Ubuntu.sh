ae2dba125c2aa76ebd01e25294e087f797d8733f
ninodemeterko <ninodemeterko@hotmail.com>
2021-08-16 03:51:26 -0300

    added sudo

--- scripts/Ubuntu.sh
+++ scripts/Ubuntu.sh
@@ -6,7 +6,7 @@ sudo apt-add-repository ppa:fish-shell/release-3
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y apt-fast
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y apt-fast
 echo debconf apt-fast/maxdownloads string 16 | debconf-set-selections
 echo debconf apt-fast/dlflag boolean false | debconf-set-selections
 echo debconf apt-fast/aptmanager string apt-get | debconf-set-selections
