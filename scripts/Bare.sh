95575f68c6971a5737277d2e9d7f7c568a36ba5f
ninodemeterko <ninodemeterko@hotmail.com>
2021-08-16 03:24:56 -0300

    fixed improper alias

--- scripts/Bare.sh
+++ scripts/Bare.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 echo ".dotfiles" >> .gitignore
 git clone --bare https://github.com/ninodemeterko/dotfiles $HOME/.dotfiles
-config config --local status.showUntrackedFiles no
+/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
