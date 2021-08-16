5bea81689db6cf56e9c9626ed9a9f8147768d0e7
ninodemeterko <ninodemeterko@hotmail.com>
2021-08-16 03:37:14 -0300

    fix

--- scripts/Bare.sh
+++ scripts/Bare.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-echo ".dotfiles" >> .gitignore
 git clone --bare https://github.com/ninodemeterko/dotfiles $HOME/.dotfiles
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
+echo ".dotfiles" >> .gitignore
+/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME pull
