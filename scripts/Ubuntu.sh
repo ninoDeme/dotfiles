dd527ef0bc6ed37d25929a04fa20c09811c0a4ca
ninodemeterko <ninodemeterko@hotmail.com>
2021-08-16 04:05:15 -0300

    changed

--- scripts/Ubuntu.sh
+++ scripts/Ubuntu.sh
@@ -8,9 +8,9 @@ echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sou
 sudo apt-get update
 sudo apt-get install -y apt-fast
 sudo /usr/bin/apt-fast install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 fish vim neovim emacs python3-neovim vifm spotify-client rustc cargo libasound2-dev libssl-dev exa bat htop wget neofetch firefox
+cd ~/Downloads/
 wget https://dl.discordapp.net/apps/linux/0.0.12/discord-0.0.12.deb
 sudo apt install ./discord-0.0.12.deb
-cd ~/Downloads/
 git clone https://github.com/salman-abedin/devour.git && cd devour && sudo make install
 cd ..
 git clone https://github.com/alacritty/alacritty
@@ -20,8 +20,7 @@ sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
 sudo desktop-file-install extra/linux/Alacritty.desktop
 sudo update-desktop-database
 sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
-cd $SCRIPT_DIR
-rm -r -f $SCRIPT_DIR/alacritty
+cd ..
 chsh -s /usr/bin/fish
 /usr/bin/fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
 /usr/bin/fish -c "fisher install ilancosman/tide"
