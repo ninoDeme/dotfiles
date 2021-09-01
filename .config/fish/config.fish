c738f70eaa60768c0efb0323a7184ef4f33668a0
ninodemeterko <ninodemeterko@hotmail.com>
2021-09-01 18:05:22 -0300

    changed fish to use nvim instead of emacs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,11 +9,11 @@ if status is-interactive
 	alias l='exa -l --group-directories-first'
 	alias le='exa --group-directories-first'
 	alias lt='exa -aT --group-directories-first'
-    if test $XDG_SESSION_TYPE = 'x11'
-        alias em='devour emacsclient -c -a=\"\"'
-    else
-        alias em='emacsclient -c -a=\"\"'
-    end
+	if test $XDG_SESSION_TYPE = 'x11'
+		alias em='devour emacsclient -c -a=\"\"'
+	else
+		alias em='emacsclient -c -a=\"\"'
+	end
 	alias emt='emacsclient -nw -a=\"\"'
 	alias cp='cp -i'
 	alias rm='rm -i'
@@ -26,8 +26,9 @@ set --global --export FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --heig
 fish_default_key_bindings
 alias rr='curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash'
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-set EDITOR "emacsclient -t -a ''"                 # $EDITOR use Emacs in terminal
-set VISUAL "devour emacsclient -c -a emacs"              # $VISUAL use Emacs in GUI mode
+# set EDITOR "emacsclient -t -a ''"                 # $EDITOR use Emacs in terminal
+# set VISUAL "devour emacsclient -c -a emacs"              # $VISUAL use Emacs in GUI mode
+set EDITOR "nvim"
 set -x MANPAGER "nvim -c 'set ft=man' -"
 fish_add_path $HOME/.emacs.d/bin
 fish_add_path $HOME/.cargo/bin
