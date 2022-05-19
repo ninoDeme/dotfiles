b006ad2a573940edbf60d9efc84575f0488e61be
ninodemeterko <ninodemeterko@hotmail.com>
2022-05-19 17:30:32 -0300

    gtk_nocsd and bitwarden support, changes in latte

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,8 +15,13 @@ if status is-interactive
 	alias cp='cp -i'
 	alias rm='rm -i'
 	alias mv='mv -i'
-	function mkexec
-	    command chmod u+x
+	function fconf
+	    command nvim $HOME/.config/fish/config.fish
+	end
+	function nvimconf
+	    pushd $HOME/.config/nvim/
+	    command nvim init.lua
+	    popd
 	end
 end
 set --global --export FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap'
@@ -25,7 +30,12 @@ alias rr='curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/mas
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 # set EDITOR "emacsclient -t -a ''"                 # $EDITOR use Emacs in terminal
 # set VISUAL "devour emacsclient -c -a emacs"              # $VISUAL use Emacs in GUI mode
-set -x QT_QPA_PLATFORMTHEME "kvantum"
+if test -e $HOME/.config/fish/gtk_nocsd.fish
+	source $HOME/.config/fish/gtk_nocsd.fish
+end
+if test -e $HOME/.config/fish/bitwarden.fish
+	source $HOME/.config/fish/bitwarden.fish
+end
 set EDITOR "nvim"
 set -x MANPAGER "nvim -R -c 'set ft=man' -"
 set -x XDG_DATA_DIRS " /usr/local/share/:/usr/share/:/var/lib/flatpak/exports/share/:$HOME/.local/share/flatpak/exports/share"
