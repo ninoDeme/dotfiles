03051229cd04b2ef9a34d01fab43b3e9c0450b11
ninodemeterko <ninodemeterko@hotmail.com>
2022-06-22 06:26:00 -0300

    work on emacs config, removed old config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,75 +9,98 @@
 ;; Change font
 (set-face-attribute 'default nil :font "NotoMono Nerd font" :height 110)
 
+
 ;; Set theme
 (load-theme 'wombat)
 
 ;; Make ESC quit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+    (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-  ;; Packages ======================================================================
+    (add-hook 'prog-mode-hook 'menu-bar--display-line-numbers-mode-relative)
 
-;; Set up package.el to work with MELPA
-(require 'package)
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+    ;; Packages ======================================================================
 
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
+    ;; Set up package.el to work with MELPA
+    (require 'package)
+    (setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			    ("org" . "https://orgmode.org/elpa/")
+			    ("elpa" . "https://elpa.gnu.org/packages/")))
 
-;; Download and install use-package
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-(eval-when-compile
-  (require 'use-package))
-(setq use-package-always-ensure t)
+    (package-initialize)
+    (unless package-archive-contents
+    (package-refresh-contents))
 
-;; install packages
-(use-package evil ;; Vim mode
-  :config
-  (evil-mode 1))
+    ;; Download and install use-package
+    (unless (package-installed-p 'use-package)
+    (package-install 'use-package))
+    (eval-when-compile
+    (require 'use-package))
+    (setq use-package-always-ensure t)
 
-(use-package evil-commentary
-  :config
-  (evil-commentary-mode))
+    ;; install packages
 
-(define-key evil-insert-state-map (kbd "M-h") 'evil-normal-state)
-(define-key evil-insert-state-map (kbd "M-j") (lambda () (interactive) (evil-normal-state) (evil-next-line)))
-(define-key evil-insert-state-map (kbd "M-k") (lambda () (interactive) (evil-normal-state) (evil-previous-line)))
-(define-key evil-insert-state-map (kbd "M-l") (lambda () (interactive) (evil-normal-state) (evil-forward-char 2)))
+    (use-package undo-fu)
 
-(use-package swiper)
+    (use-package evil ;; Vim mode
+    :init
+    (setq evil-undo-system 'undo-fu)
+    :config
+    (evil-mode 1))
 
-;; ivy completions
-(use-package ivy
-  :diminish
-  :bind (("C-s" . swiper)
-         :map ivy-minibuffer-map
-         ("C-l" . ivy-alt-done)
-         ("C-j" . ivy-next-line)
-	 ("<up>" . ivy-previous-history-element)
-	 ("<down>" . ivy-next-history-element)
-         ("C-k" . ivy-previous-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t)
-  (setq ivy-count-format "(%d/%d) "))
+    (use-package evil-commentary
+    :config
+    (evil-commentary-mode))
+
+    (define-key evil-insert-state-map (kbd "M-h") 'evil-normal-state)
+    (define-key evil-insert-state-map (kbd "M-j") (lambda () (interactive) (evil-normal-state) (evil-next-line)))
+    (define-key evil-insert-state-map (kbd "M-k") (lambda () (interactive) (evil-normal-state) (evil-previous-line)))
+    (define-key evil-insert-state-map (kbd "M-l") (lambda () (interactive) (evil-normal-state) (evil-forward-char 2)))
+
+    ;; ivy completions
+    (use-package swiper)
+    (use-package counsel
+	:config
+	(counsel-mode 1))
+    (use-package ivy 
+	:diminish
+	:init (define-prefix-command 'C-s-map)
+	    (global-set-key (kbd "C-s") 'C-s-map)
+	:bind (("C-s s" . swiper)
+	    ("C-s C-s" . swiper)
+	    ("C-s b" . counsel-switch-buffer)
+	    ("C-s C-b" . counsel-switch-buffer)
+	    :map ivy-minibuffer-map
+	    ("C-l" . ivy-alt-done)
+	    ("C-j" . ivy-next-line)
+	    ("<tab>" . ivy-next-line)
+	    ("<up>" . ivy-previous-history-element)
+	    ("<down>" . ivy-next-history-element)
+	    ("C-k" . ivy-previous-line)
+	    :map ivy-switch-buffer-map
+	    ("C-k" . ivy-previous-line)
+	    ("C-l" . ivy-done)
+	    ("C-d" . ivy-switch-buffer-kill)
+	    :map ivy-reverse-i-search-map
+	    ("C-k" . ivy-previous-line)
+	    ("C-d" . ivy-reverse-i-search-kill))
+	:config
+	(ivy-mode 1)
+	(setq ivy-use-virtual-buffers t)
+	(setq ivy-count-format "(%d/%d) "))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil-commentary swiper ivy use-package evil)))
+ '(package-selected-packages
+   '(counsel doom-modeline evil-commentary swiper ivy use-package evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
