53fbc5f4b23c33992bcc1abafe24bb66eaa88d86
Ricardo Soares Demeterko <60797244+ninoDeme@users.noreply.github.com>
2022-06-24 05:52:03 -0300

    Changed username

--- scripts/Bare.sh
+++ scripts/Bare.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-git clone --bare https://github.com/ninodemeterko/dotfiles $HOME/.dotfiles
+git clone --bare https://github.com/ninoDeme/dotfiles $HOME/.dotfiles
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
 echo ".dotfiles" >> .gitignore
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME reset --hard HEAD
