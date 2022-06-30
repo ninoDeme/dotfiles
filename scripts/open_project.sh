c27a4aaab28a7e9f6a5ebd9529d651c791ebeff7
ninodemeterko <ninodemeterko@hotmail.com>
2022-06-30 00:15:45 -0300

    Changed polybar font

--- scripts/open_project.sh
+++ scripts/open_project.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-DTERM="kitty" #Set default terminal
+[ -z "$DEFAULTTERM" ] && DEFAULTTERM="kitty" #Set default terminal
 
 # Chose editor
 chosen=$(printf "Emacs\nVSCode\nNeovim\nTerminal" | rofi -dmenu -i -p "Editor:")
@@ -8,13 +8,13 @@ chosen=$(printf "Emacs\nVSCode\nNeovim\nTerminal" | rofi -dmenu -i -p "Editor:")
 case "$chosen" in
     "Emacs") edit="emacs" ;;
     "VSCode") edit="code" ;;
-    "Neovim") [ -t 1 ] && edit="nvim" || edit="$DTERM -e nvim" ;;
-    "Terminal") edit="$DTERM -d" ;;
+    "Neovim") [ -t 1 ] && edit="nvim" || edit="$DEFAULTTERM -e nvim" ;;
+    "Terminal") edit="$DEFAULTTERM -d" ;;
     *) exit 1 ;;
 esac
 
 # Set project list in format "<name that will appear on rofi>,<work dir>,<main file or use dot to use work dir>"
-projects=("qtile,$HOME/.config/qtile,config.py" "AwesomeWM,$HOME/.config/awesome,rc.lua" "XMonad,$HOME/.config/xmonad,xmonad.hs" "Rofi,$HOME/.config/rofi,config.rasi" "Neovim,$HOME/.config/nvim,init.lua" "scripts,$HOME/scripts,." "Emacs,$HOME/.emacs.d,init.el" "Fish_Shell,$HOME/.config/fish,config.fish" "Bash,$HOME,.bashrc")
+projects=("qtile,$HOME/.config/qtile,config.py" "AwesomeWM,$HOME/.config/awesome,rc.lua" "XMonad,$HOME/.config/xmonad,xmonad.hs" "Rofi,$HOME/.config/rofi,config.rasi" "Neovim,$HOME/.config/nvim,init.lua" "scripts,$HOME/scripts,." "Emacs,$HOME/.emacs.d,init.el" "Fish\x20Shell,$HOME/.config/fish,config.fish" "Bash,$HOME,.bashrc")
 
 # Generate project prompt
 rofi_prompt=""
@@ -31,6 +31,7 @@ exec_cmd=$(printf "$rofi_prompt" | rofi -dmenu -i -p "Project:")
 projectIndex=(${exec_cmd//: / })
 tmpVar=${projectIndex[0]}
 tmpArray=(${projects[$(($tmpVar - 1))]//,/ })
+
 case "$chosen" in 
 	"VSCode") cd ${tmpArray[1]} && $edit ${tmpArray[2]} . ;;
 	"Terminal") $edit ${tmpArray[1]} ;;
