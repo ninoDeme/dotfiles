9ea23ae3b558f05a227ea1f1fa819a48ba98b3c0
ricardo.demeterko <ninodemeterko@hotmail.com>
2024-05-13 17:48:38 -0300

    hhhhhhm

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -25,6 +25,8 @@ return {
         ['<C-y>'] = cmp.mapping.scroll_docs(-1),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-l>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
       })
       cmp.setup {
         window = {
