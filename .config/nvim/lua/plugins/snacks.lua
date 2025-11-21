241086330229da9a00aa3ee370cc495f3f51a9f7
Ricardo S. Demeterko <mail@ninodeme.com>
2025-11-21 18:06:13 -0300

    fetch all branches nvim snack picker

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -155,7 +155,8 @@ return {
     { "<leader>sp",      function() Snacks.picker.projects() end,           desc = "Projects" },
     { "<leader>sr",      function() Snacks.picker.recent() end,             desc = "Recent" },
     -- git
-    { "<leader>gb",      function() Snacks.picker.git_branches() end,       desc = "Git Branches" },
+    { "<leader>gb",      function() Snacks.picker.git_branches({ all = true }) end,       desc = "Git Branches" },
+    { "<leader>gB",      function() Snacks.picker.git_branches() end,       desc = "Git Branches (local only)" },
     { "<leader>gl",      function() Snacks.picker.git_log() end,            desc = "Git Log" },
     { "<leader>gL",      function() Snacks.picker.git_log_line() end,       desc = "Git Log Line" },
     -- { "<leader>Gs",       function() Snacks.picker.git_status() end,         desc = "Git Status" },
